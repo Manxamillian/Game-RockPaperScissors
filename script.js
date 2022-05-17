73ef1d2e355c83ccaab0ce2e2b426831ed12b17f
manxamillian <manxamillian@gmail.com>
2022-05-17 14:02:56 -0500

    game works with hardcoded player value

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Rock / Paper / Scissors game
 
-const initialPlayerSelection = "rock";             //TEMP -- hardcoded playerSelection value - need to set up input from user (HTML) 
-const playerSelection = capitalize(initialPlayerSelection);  //change user input to all uppercase
+const initialPlayerSelection = "paper";             //TEMP -- hardcoded playerSelection value - need to set up input from user (HTML) 
+const playerSelection = capitalize(initialPlayerSelection);  //change user input to 'first letter in caps, the rest lowercase
 console.log(playerSelection);
 
+
+
+
+
 function capitalize(initialPlayerSelection) {
-    return initialPlayerSelection.toUpperCase();    
+    return initialPlayerSelection.charAt(0).toUpperCase() + initialPlayerSelection.slice(1).toLowerCase();    
 }
 
 const computerSelection = computerPlay();     //function returns random result for the computer player
@@ -16,12 +20,12 @@ function computerPlay(){
     let randomChoice = getRandomNum(3);   //request random number, either 0, 1, or 2
     
     if (randomChoice == 0) {      //using randomChoice value (0,1,or 2) to match corresponding string value
-        return ('ROCK');                
+        return ('Rock');                
     } else {
         if (randomChoice == 1) {
-            return ('PAPER');                        
+            return ('Paper');                        
         } else {
-            return ('SCISSORS');                            
+            return ('Scissors');                            
         }
     }      
 }
@@ -30,28 +34,35 @@ function getRandomNum(max) {        //passed function the number (3) from comput
     return Math.floor(Math.random() * max);    
 }
 
+playRound(playerSelection, computerSelection);
+
 function playRound(playerSelection, computerSelection){      //play a round using the player input and the random computerSelection 
-    //set the playerSelection string value to all uppercase
-    // const playerSelection = "rock";    //hardcoding playerSelection value - need input in HTML for true value - TEMP
-    playerValueUppercase();
+    //not checking here for invalid input for playerSelection.  Use images/buttons for options instead
+    let playerWins;
 
-    if (playerSelection !== computerSelection) {  //note: not checking here for invalid input for playerSelection.  Use images instead
-
-        //check player win values here - there are 3 cases
-        //playerSelection == 'ROCK' && computerSelection == 'SCISSORS'
-        //playerSelection == 'PAPER' && computerSelection == 'ROCK'
-        //playerSelection == 'SCISSORS' && computerSelection == 'PAPER'
-
-        if (____){    
-            return (`You Lose! ${computerSelection} beats ${playerSelection}`);  //return if computer wins
-        } else {
-            return (`You Win! ${playerSelection} beats ${computerSelection}`);   //return if player wins
-        }
+    if (playerSelection == computerSelection) {
+        console.log('tie');
+        return ("It's a tie!  Try again.");            // TIE game
     } else {
-        return ("It's a tie!  Try again.");    //return if it's a tie
-    }
-      
-    
+        
+        console.log(playerWins);
+        console.log(playerSelection, computerSelection);   //check value before IF statement is run
+        if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||  
+            (playerSelection == 'Paper' && computerSelection == 'Rock')  ||
+            (playerSelection == 'Scissors' && computerSelection == 'Paper')) {  //check if player is the winner                
+            
+            playerWins = 'true';                
+        } 
+            console.log(playerWins);
+
+        if (playerWins == 'true'){ 
+            console.log('winner');
+            return (`You Win! ${playerSelection} beats ${computerSelection}`);   //return if player wins               
+        } else {
+            console.log('loser');
+            return (`You Lose! ${computerSelection} beats ${playerSelection}`);  //return if computer wins            
+        }
+    }    
 }
 
 
